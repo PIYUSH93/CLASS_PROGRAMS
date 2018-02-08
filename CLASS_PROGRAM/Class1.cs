32edc80b138202980a9645f0d26549132101a3d7
Piyush Jena <piyush.jena2011@gmail.com>
2018-02-08 17:46:47 +0530

    CLASS_PROGRAM

---
+++ CLASS_PROGRAM/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_PROGRAM
+{
+    class Calculation
+    {
+        public int a;
+        public int b;
+        public int res;
+        public void display()
+        {
+            res = a + b;
+        }
+    }
+}
---
+++ CLASS_PROGRAM/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLASS_PROGRAM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculation ob1 = new Calculation();
+            ob1.a = 10;
+            ob1.b = 20;
+            ob1.display();
+            textBox1.Text = ob1.res.ToString();
+        }
+    }
+}
